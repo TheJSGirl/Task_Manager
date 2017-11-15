50f8887afcb1116452b4e7d719ec7e29eb902e3b
Priyanka Negi <prinegi100@gmail.com>
2017-11-15 14:31:54 +0530

    styled to text

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -10,8 +10,8 @@ class TodoItem extends React.Component{
     return(
       <div className = "row center">
         <div className = "col s12 m10 l10 offset-l1 offset-m1 ">
-          <li  className= "list-group-item card-panel teal lighten-2" >{todo.text} &nbsp;
-          <button  className="waves-effect waves-light btn" onClick ={this.deleteItemFromState}>Delete</button>
+          <li  className= "list-group-item card-panel  flow-text" >{todo.text} &nbsp;
+          <button  className="waves-effect waves-light btn  pink darken-3" onClick ={this.deleteItemFromState}>Delete</button>
       
       </li>
         </div>
