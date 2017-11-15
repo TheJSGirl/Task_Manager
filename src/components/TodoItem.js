839d29eeb250ff1cc7e2f37e5d5ad890486e8234
Priyanka Negi <prinegi100@gmail.com>
2017-11-15 14:18:33 +0530

    made responsive lists and given style to list element

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -9,7 +9,7 @@ class TodoItem extends React.Component{
     const {todo} = this.props;
     return(
       <div className = "row center">
-        <div className = "col s12 m6 l6 ">
+        <div className = "col s12 m10 l10 offset-l1 offset-m1 ">
           <li  className= "list-group-item card-panel teal lighten-2" >{todo.text} &nbsp;
           <button  className="waves-effect waves-light btn" onClick ={this.deleteItemFromState}>Delete</button>
       
